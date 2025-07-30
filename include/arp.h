a0d447a9a3d927854571306bd6d3abbf932e0d47
Julecko <julecko@dilino.sk>
2025-07-30 13:38:23 +0200

    Documented my code

--- include/arp.h
+++ include/arp.h
@@ -7,12 +7,12 @@
 int get_interface_mac(const char *iface_name, uint8_t *mac_out);
 int arp_send_reply(const char *iface_name,
                    const uint8_t *src_mac,
-                   const char *src_ip,
+                   const uint32_t *src_ip,
                    const uint8_t *dest_mac,
-                   const char *dest_ip);
+                   const uint32_t *dest_ip);
 int arp_send_request(const char *iface_name,
                      const uint8_t *src_mac,
-                     const char *src_ip,
-                     const char *target_ip);
+                     const uint32_t *src_ip,
+                     const uint32_t *target_ip);
 
 #endif
--- src/arp.c
+++ src/arp.c
@@ -17,17 +17,19 @@
 #include <net/if_arp.h>
 
 #define ARP_PACKET_SIZE 42
+#define MAC_LEN 6
+#define IP_LEN 4
 
 struct arp_hdr {
-    uint16_t htype;
-    uint16_t ptype;
-    uint8_t hlen;
-    uint8_t plen;
-    uint16_t oper;
-    uint8_t sha[6];
-    uint8_t spa[4];
-    uint8_t tha[6];
-    uint8_t tpa[4];
+    uint16_t htype;         // Hardware type (Ethernet 0x0001)
+    uint16_t ptype;         // Protocol type (IPv4 0x0800)
+    uint8_t hlen;           // Hardware length (Mac 6)
+    uint8_t plen;           // Protocol length (IPv4 4)
+    uint16_t oper;          // Operation (1 Request 2 Response)
+    uint8_t sha[MAC_LEN];   // Sender hardware address (AA:BB:CC:DD:FF)
+    uint8_t spa[IP_LEN];    // Sender protocol address (192.168.0.1)
+    uint8_t tha[MAC_LEN];   // Target hardware address (AA:BB:CC:DD:FF)
+    uint8_t tpa[IP_LEN];    // Target protocol address (192.168.0.1)
 } __attribute__((packed));
 
 int get_interface_index(int sockfd, const char *iface_name) {
@@ -55,7 +57,6 @@ int get_interface_mac(const char *iface_name, uint8_t *mac_out) {
     return 0;
 }
 
-// Setup Ethernet header
 void set_ethernet_header(struct ether_header *eth,
                          const uint8_t *dest_mac,
                          const uint8_t *src_mac,
@@ -65,33 +66,31 @@ void set_ethernet_header(struct ether_header *eth,
     eth->ether_type = htons(ethertype);
 }
 
-// Setup ARP header
 void set_arp_header(struct arp_hdr *arp,
                     uint16_t oper,
                     const uint8_t *sender_mac,
-                    const char *sender_ip_str,
+                    const uint32_t *sender_ip,
                     const uint8_t *target_mac,
-                    const char *target_ip_str) {
+                    const uint32_t *target_ip) {
     arp->htype = htons(1);          // Ethernet
     arp->ptype = htons(ETH_P_IP);   // IPv4
-    arp->hlen = 6;                  // MAC length
-    arp->plen = 4;                  // IPv4 length
+    arp->hlen = MAC_LEN;            // MAC length
+    arp->plen = IP_LEN;             // IPv4 length
     arp->oper = htons(oper);        // Request or Reply
 
-    memcpy(arp->sha, sender_mac, 6);
-    inet_pton(AF_INET, sender_ip_str, arp->spa);
-    memcpy(arp->tha, target_mac, 6);
-    inet_pton(AF_INET, target_ip_str, arp->tpa);
+    memcpy(arp->sha, sender_mac, MAC_LEN);
+    memcpy(arp->spa, sender_ip, IP_LEN);
+    memcpy(arp->tha, target_mac, MAC_LEN);
+    memcpy(arp->tpa, target_ip, IP_LEN);
 }
 
-// Main function to send ARP packet
 int send_arp_packet(const char *iface_name,
                     const uint8_t *dest_mac,
-                    const uint8_t *src_mac,
                     uint16_t arp_oper,
-                    const char *sender_ip,
+                    const uint8_t *sender_mac,
+                    const uint32_t *sender_ip,
                     const uint8_t *target_mac,
-                    const char *target_ip) {
+                    const uint32_t *target_ip) {
 
     int sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
     if (sockfd < 0) {
@@ -109,8 +108,8 @@ int send_arp_packet(const char *iface_name,
     struct ether_header *eth = (struct ether_header *)buffer;
     struct arp_hdr *arp = (struct arp_hdr *)(buffer + sizeof(struct ether_header));
 
-    set_ethernet_header(eth, dest_mac, src_mac, ETH_P_ARP);
-    set_arp_header(arp, arp_oper, src_mac, sender_ip, target_mac, target_ip);
+    set_ethernet_header(eth, dest_mac, sender_mac, ETH_P_ARP);
+    set_arp_header(arp, arp_oper, sender_mac, sender_ip, target_mac, target_ip);
 
     struct sockaddr_ll addr = {0};
     addr.sll_family = AF_PACKET;
@@ -131,39 +130,36 @@ int send_arp_packet(const char *iface_name,
     return 0;
 }
 
-#include <net/if_arp.h>  // for ARPOP_REQUEST and ARPOP_REPLY
-
-// Sends an ARP Request to ask "Who has target_ip? Tell me!"
 int arp_send_request(const char *iface_name,
                      const uint8_t *src_mac,
-                     const char *src_ip,
-                     const char *target_ip) {
+                     const uint32_t *src_ip,
+                     const uint32_t *target_ip) {
     uint8_t broadcast_mac[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
     uint8_t zero_mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     
     return send_arp_packet(
         iface_name,
         broadcast_mac,     // destination MAC (broadcast)
-        src_mac,           // source MAC
         ARPOP_REQUEST,     // ARP operation
-        src_ip,
+        src_mac,           // source MAC
+        src_ip,            // src_ip
         zero_mac,          // target MAC is unknown
-        target_ip
+        target_ip          // target IP
     );
 }
 
 int arp_send_reply(const char *iface_name,
                    const uint8_t *src_mac,
-                   const char *src_ip,
+                   const uint32_t *src_ip,
                    const uint8_t *dest_mac,
-                   const char *dest_ip) {
+                   const uint32_t *dest_ip) {
     return send_arp_packet(
         iface_name,
         dest_mac,          // destination MAC (target host)
-        src_mac,           // source MAC (us)
         ARPOP_REPLY,       // ARP operation
-        src_ip,
+        src_mac,           // source MAC (us)
+        src_ip,            // source IP (spoofed target)
         dest_mac,          // target MAC (the one we're replying to)
-        dest_ip
+        dest_ip            // Target IP (the one we're replying to)
     );
 }
